1fc694579c74c973f8b0deef67f588c778d8b82d
Kniff2503 <Kniff2503@gmail.com>
2019-01-15 21:42:07 +0100

    adding POJOs. Closes #2.

---
+++ src/main/java/com/semjasa/vernocsyndicates/models/Post.java
@@ -0,0 +1,43 @@
+package com.semjasa.vernocsyndicates.models;
+
+import java.time.LocalDate;
+
+public class Post {
+
+    public String headline;
+    public String content;
+    public User user;
+    public LocalDate postDate;
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDate getPostDate() {
+        return postDate;
+    }
+
+    public void setPostDate(LocalDate postDate) {
+        this.postDate = postDate;
+    }
+}
---
+++ src/main/java/com/semjasa/vernocsyndicates/models/User.java
@@ -0,0 +1,37 @@
+package com.semjasa.vernocsyndicates.models;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private String rank;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+}
