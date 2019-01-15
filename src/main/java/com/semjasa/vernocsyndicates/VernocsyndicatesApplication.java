d9bed89266bab8945b19f270d9cbde57ffb984fd
Thomas Heise <Thomas.Heise@icamsystems.de>
2019-01-15 10:55:01 +0100

    Initialize Project

---
+++ src/main/java/com/semjasa/vernocsyndicates/VernocsyndicatesApplication.java
@@ -0,0 +1,14 @@
+package com.semjasa.vernocsyndicates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VernocsyndicatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VernocsyndicatesApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/semjasa/vernocsyndicates/VernocsyndicatesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.semjasa.vernocsyndicates;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VernocsyndicatesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
