45e7843c12a45bb88130e1c749de13aebd4dda25
Kniff2503 <Kniff2503@gmail.com>
2019-01-17 21:50:29 +0100

    implement map based service. closes #6.

---
+++ vernocsyndicates-data/src/main/java/com/semjasa/vernocsyndicates/services/maps/PostServiceMap.java
@@ -0,0 +1,33 @@
+package com.semjasa.vernocsyndicates.services.maps;
+
+import com.semjasa.vernocsyndicates.models.Post;
+import com.semjasa.vernocsyndicates.services.PostService;
+
+import java.util.Set;
+
+public class PostServiceMap extends AbstractMapService<Post, Long> implements PostService<Post, Long> {
+    @Override
+    public Set<Post> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Post object) {
+        super.deleteById(object);
+    }
+
+    @Override
+    public Post save(Post object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Post findById(Long id) {
+        return super.findById(id);
+    }
+}
