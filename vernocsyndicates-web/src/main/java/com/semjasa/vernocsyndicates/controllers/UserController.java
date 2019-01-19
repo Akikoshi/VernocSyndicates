a8e3578dc98a8fec86e3774e4485b2aedefecd3b
Kniff2503 <Kniff2503@gmail.com>
2019-01-19 14:12:19 +0100

    adding in list all users on index Page. closes #19.

--- vernocsyndicates-web/src/main/java/com/semjasa/vernocsyndicates/controllers/UserController.java
+++ vernocsyndicates-web/src/main/java/com/semjasa/vernocsyndicates/controllers/UserController.java
@@ -1,12 +1,21 @@
 package com.semjasa.vernocsyndicates.controllers;
 
+import com.semjasa.vernocsyndicates.services.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @RequestMapping({"/users","/users/indes","/users/index.html"})
-    public String listUser(){
+    public String listUser(Model model){
+        model.addAttribute("users",userService.findAll());
         return "users/index";
     }
 }
