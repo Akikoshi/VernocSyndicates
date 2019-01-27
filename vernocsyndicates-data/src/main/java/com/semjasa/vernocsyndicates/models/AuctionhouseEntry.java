5f44806d198f6bb4294f25a8533ac0ebcb2bf445
Kniff2503 <Kniff2503@gmail.com>
2019-01-27 13:39:07 +0100

    Implement Auctionhouse Entry. Closes #51

---
+++ vernocsyndicates-data/src/main/java/com/semjasa/vernocsyndicates/models/AuctionhouseEntry.java
@@ -0,0 +1,10 @@
+package com.semjasa.vernocsyndicates.models;
+
+public class AuctionhouseEntry extends BaseEntity {
+    public String itemName;
+    public Integer costs;
+    public String playerName;
+    public Integer amount;
+    public OnlineState onlineState;
+    public SellBuyState sellBuyState;
+}
